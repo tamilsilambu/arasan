ca6cbae39a13a13a3baca64d0398939bb816ccb1
tamilsilambu <tamilmech47@gmail.com>
2024-03-13 17:19:16 +0530

    day3

--- src/test/java/org/git/Sample.java
+++ src/test/java/org/git/Sample.java
@@ -4,6 +4,6 @@ public class Sample {
 public static void main(String[] args) {
 	System.out.println("day1");
 	System.out.println("day2");
-	System.out.println("day3");
+	System.out.println("day4");
 }
 }
